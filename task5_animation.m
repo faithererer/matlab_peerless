5ac552b730679ebe080f645618eedf6ed511d715
晓破千年 <qq_62228785@gitcode.net>
2024-12-16 19:50:37 +0800

    c:\Users\86136\AppData\Roaming\SyncClipboard\file\20241216\temp images\swokym4e.cfq.png

--- task5_animation.m
+++ task5_animation.m
@@ -2,14 +2,11 @@
 % 题目5：沿路径运动的动画演示
 
 function task5_animation
-    % 创建图形窗口
-    figure('Position', [100 100 800 600]);
-    ax = gca;
-    
-    % 设置坐标轴
-    ax.XLim = [-0.5 2.5];
-    ax.YLim = [-0.5 2.5];
-    ax.DataAspectRatio = [1 1 1];
+    % 设置图形窗口
+    set(gca, 'XLim', [-0.5 2.5], 'YLim', [-0.5 2.5], ...
+        'Drawmode', 'fast', 'Visible', 'on');
+    cla
+    axis square
     grid on;
     hold on;
     
@@ -19,27 +16,25 @@ function task5_animation
     y_plot = 1.5 + 0.3*t_plot + 0.9*t_plot.^2 - 2.7*t_plot.^3;
     plot(x_plot, y_plot, 'b-', 'LineWidth', 1);
     
-    % 创建运动点（使用持久化的图形对象）
+    % 创建运动点
     ball = plot(x_plot(1), y_plot(1), 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
     
-    % 原始参数速度的动画
+    % 原始参数运动
     title('Motion with Original Parameterization');
     for t = linspace(0, 1, 100)
         % 计算当前位置
         x = 0.5 + 0.3*t + 3.9*t^2 - 4.7*t^3;
         y = 1.5 + 0.3*t + 0.9*t^2 - 2.7*t^3;
         
-        % 更新点的位置
-        set(ball, 'XData', x, 'YData', y);
+        % 使用题目要求的命令更新位置
+        set(ball, 'xdata', x, 'ydata', y);
         drawnow;
         pause(0.01);
     end
     pause(1);
     
-    % 等速运动的动画
+    % 等速运动
     title('Motion with Constant Speed');
-    total_length = task1_arc_length(1);
-    
     for s = linspace(0, 1, 100)
         % 找到对应的参数t
         t = task2_find_t(s);
@@ -48,8 +43,8 @@ function task5_animation
         x = 0.5 + 0.3*t + 3.9*t^2 - 4.7*t^3;
         y = 1.5 + 0.3*t + 0.9*t^2 - 2.7*t^3;
         
-        % 更新点的位置
-        set(ball, 'XData', x, 'YData', y);
+        % 使用题目要求的命令更新位置
+        set(ball, 'xdata', x, 'ydata', y);
         drawnow;
         pause(0.01);
     end
